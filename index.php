e234b66454f822a0c312d59806966115aec0a4dc
devblin <110119035@nitt.edu>
2020-09-03 13:20:23 +0530

    as

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 // require_once __DIR__ . "/setup/connection.php";
 require_once __DIR__ . "/setup/autoload.php";
 $dir = env("FOLDER");
+$baseUrl = env("BASE_URL");
 $router = $_SERVER['REQUEST_URI'];
 $router = str_replace($dir, "", $router);
 // echo $router;
@@ -14,12 +15,17 @@ $router = str_replace($dir, "", $router);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Cache-control" content="no-cache">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="shortcut icon" href="katologo.jpeg" type="image/x-icon">
+    <link rel="stylesheet" href="cssandjs/css/index.css">
     <title>Kato</title>
 </head>
 
 <body>
+    <input id="baseurl" type="hidden" value=<?php echo $baseUrl; ?>>
     <?php
     if (!isset($_SESSION['USER'])) {
         switch ($router) {
@@ -39,9 +45,14 @@ $router = str_replace($dir, "", $router);
     } else {
     }
     ?>
+    <footer style="position: absolute;">&copy; devblin</footer>
 </body>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+let baseUrl = $("#baseurl").val();
+</script>
 
 </html>
--- user/login.php
+++ user/login.php
@@ -1,1 +1,23 @@
-<h1>LOGIN</h1>
+<div class="container-fluid text-center max400 d-flex flex-column">
+    <h1 class="m-5">Login</h1>
+    <form>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text logobgcolor text-white">
+                    <i class="fas fa-user"></i></span>
+            </div>
+            <input type="text" class="form-control" placeholder="Username">
+        </div>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class=" input-group-text logobgcolor text-white">
+                    <i class=" fas fa-key"></i></span>
+            </div>
+            <input type="password" class="form-control" placeholder="Password">
+        </div>
+
+        <button class="btn btn-dark w-100 logobgcolor p-2">Login</button>
+        <p class="m-2">New User? <a class="logocolor" href=<?php echo $baseUrl . "/Register" ?>>Register</a> <br>
+            Forgot Password? <a class="logocolor" href=<?php echo $baseUrl . "/ResetPassword"; ?>>Reset</a></p>
+    </form>
+</div>
