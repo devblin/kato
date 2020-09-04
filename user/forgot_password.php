b136d2a666cb5959874d429f1496e188a93771a6
devblin <110119035@nitt.edu>
2020-09-05 00:38:08 +0530

    addded

--- user/forgot_password.php
+++ user/forgot_password.php
@@ -4,14 +4,14 @@ echo "<title>Kato | Reset Password</title>";
 <div class="container-fluid text-center max400 d-flex flex-column">
     <h1 class="ml-0 mr-0  mb-5 mt-5">Password Reset </h1>
     <form>
-        <div class="input-group mb-3">
+        <div class="input-group mb-2">
             <div class="input-group-prepend">
                 <span class="input-group-text logobgcolor text-white">
                     <i class="fas fa-user"></i></span>
             </div>
             <input id="forgotuname" type="text" class="form-control" placeholder="Username" required>
         </div>
-        <div class="input-group mb-3">
+        <div class="input-group mb-2">
             <div class="input-group-prepend">
                 <span class=" input-group-text logobgcolor text-white">
                     <i class=" fas fa-key"></i></span>
