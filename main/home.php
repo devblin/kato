8253f8c750a4f0a80c6d7e9be714099264763725
devblin <110119035@nitt.edu>
2020-09-05 01:22:02 +0530

    logout

--- main/home.php
+++ main/home.php
@@ -1,2 +1,8 @@
-<button class="btn btn-danger">Logout</button>
+<button class="btn btn-danger" id="logout">Logout</button>
 <h1>HOME</h1>
+<script>
+let logoutBtn = $("#logout");
+logoutBtn.click(function() {
+    window.location = baseUrl + "/user/logout.php";
+});
+</script>
