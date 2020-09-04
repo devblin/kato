3d00d37a77cd4323a9853b8dbe7e07dfa32ad15c
devblin <110119035@nitt.edu>
2020-09-04 23:27:24 +0530

    setup

--- setup/env_sample.php
+++ setup/env_sample.php
@@ -1,11 +1,11 @@
 <?php
 $variables = [
-    'DB_NAME' => 'YOUR_DB_NAME',
-    'DB_PASSWORD' => '',
-    'DB_USERNAME' => 'YOUR_DB_USERNAME',
-    'LOCAL_HOST' => 'localhost',
-    'BASE_URL' => 'http://localhost/Kato', //CHANGE IN GIVEN FORMAT
-    'FOLDER' => '/Kato' //CHANGE IN GIVEN FORMAT
+    'DB_NAME' => 'bp2eceheqxnmmhgybjjd',
+    'DB_PASSWORD' => 'seeUJYwcEedoOB14n4EF',
+    'DB_USERNAME' => 'uzeq1xikolfjksbk',
+    'LOCAL_HOST' => 'bp2eceheqxnmmhgybjjd-mysql.services.clever-cloud.com',
+    'BASE_URL' => 'https://kato2.herokuapp.com', //CHANGE IN GIVEN FORMAT
+    'FOLDER' => '' //CHANGE IN GIVEN FORMAT
 ];
 foreach ($variables as $key => $val) {
     putenv("$key=$val");
