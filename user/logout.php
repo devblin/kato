cde5c261f2929be611fcd9b3a25c2098c3693e19
devblin <110119035@nitt.edu>
2020-09-05 01:20:56 +0530

    lgoout

--- user/logout.php
+++ user/logout.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
-require_once __DIR__ . "/../functions/mysql_func.php";
+require __DIR__ . "/../functions/mysql_func.php";
+
 if ($_COOKIE['katouser'] == $_SESSION['USER']) {
     setcookie("katouser",  "", time() - 3600, "/");
     setcookie("katopassword", "", time() - 3600, "/");
 }
-$baseurl = env("BASE_URL");
 
+$baseurl = env("BASE_URL");
 session_unset();
 session_destroy();
-header("Location:" . $baseurl);
+header("Location: " . $baseurl);
