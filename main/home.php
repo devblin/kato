242291c4ff11f58162a5a9cf114911049cdf4b8b
devblin <110119035@nitt.edu>
2020-09-06 21:35:08 +0530

    my own kit

--- main/home.php
+++ main/home.php
@@ -8,16 +8,24 @@ $homeUrl = $baseUrl;
 }
 </style>
 <div class="container-fluid text-center p-0">
-    <!-- <button class="btn btn-danger" id="logout">Logout</button> -->
+    <div id="mySidepanel" class="sidepanel">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <h3 class="text-light">Welcome, <?php echo $_SESSION['NAME']; ?></h3>
+        <a href="#">Account</a>
+        <a href="#">Wishlist</a>
+        <a href="#">Cart</a>
+        <a href="#">Orders</a>
+        <a href="#" class="text-danger logout">Logout</a>
+    </div>
     <nav class="navbar navbar-expand-sm bg-dark p-1 row m-0">
         <div class="col-sm-3 col-12" style="display: contents;">
-            <button class="btn border border-light ml-1 mr-1" data-toggle="tooltip" title="More">
+            <button onclick="openNav()" class="btn border border-light ml-1 mr-1">
                 <i class="fas fa-sliders-h logocolor"></i>
             </button>
             <a class="navbar-brand text-center" href=<?php echo $homeUrl; ?>>
                 <h2 class="logocolor pl-2"><i class="fas fa-shopping-cart" id="logoi"></i> Kato</h2>
             </a>
-            <button class="btn ml-1 mr-1 f20 text-light d-sm-none d-block" data-toggle="tooltip" title="Logout">
+            <button class="logout btn ml-1 mr-1 f20 text-light d-sm-none d-block" data-toggle="tooltip" title="Logout">
                 <i class="fas fa-sign-out-alt"></i>
             </button>
         </div>
@@ -40,16 +48,43 @@ $homeUrl = $baseUrl;
                 </div>
             </div>
             <button class="btn ml-2 mr-1 f20 text-light d-sm-block d-none" data-toggle="tooltip" title="Account">
-                <i class="fas fa-user"></i>
+                <span class="d-flex align-items-center"><i class="fas fa-user"></i><span
+                        class="f15 ml-2"><?php echo $_SESSION['NAME']; ?></span></span>
             </button>
-            <button class="btn ml-1 mr-1 f20 text-light d-sm-block d-none" data-toggle="tooltip" title="Logout">
+            <button class="btn ml-1 mr-1 f20 text-light d-sm-block d-none" data-toggle="tooltip" title="Your Cart">
+                <i class="fas fa-cart-arrow-down"></i>
+            </button>
+            <button class="btn ml-1 mr-1 f20 text-light d-sm-block d-none logout" data-toggle="tooltip" title="Logout">
                 <i class="fas fa-sign-out-alt"></i>
             </button>
         </div>
     </nav>
+    <!-- <div class="container p-0 row m-auto w-auto" style="height: 84vh;overflow-y:auto">
+        <div class="col-md-4 bg-light col-sm-6 col-12 p-0">
+            <div class="bg-dark m-2 p-2 text-light">
+                Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. In
+                officia
+                nesciunt
+                ducimus
+                illum
+                impedit, nisi
+                ab distinctio veniam dicta dolore alias culpa quisquam aliquam, voluptates non sed error, laudantium
+                nihil!
+            </div>
+        </div>
+    </div> -->
 </div>
 <script>
-let logoutBtn = $("#logout");
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+let logoutBtn = $(".logout");
 logoutBtn.click(function() {
     window.location = baseUrl + "/user/logout.php";
 });
