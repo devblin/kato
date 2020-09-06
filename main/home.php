66900c8e97ef6b46893156fa4d1e906dafcf3e14
devblin <110119035@nitt.edu>
2020-09-06 18:37:06 +0530

    resolved test scroll overflow

--- main/home.php
+++ main/home.php
@@ -9,20 +9,20 @@ $homeUrl = $baseUrl;
 </style>
 <div class="container-fluid text-center p-0">
     <!-- <button class="btn btn-danger" id="logout">Logout</button> -->
-    <nav class="navbar navbar-expand-sm bg-dark p-1 row">
-        <div class="col-sm-3 col-12 " style="display: contents;">
-            <button class="btn border border-light ml-4" data-toggle="tooltip" title="More">
+    <nav class="navbar navbar-expand-sm bg-dark p-1 row m-0">
+        <div class="col-sm-3 col-12" style="display: contents;">
+            <button class="btn border border-light ml-1 mr-1" data-toggle="tooltip" title="More">
                 <i class="fas fa-sliders-h logocolor"></i>
             </button>
             <a class="navbar-brand text-center" href=<?php echo $homeUrl; ?>>
                 <h2 class="logocolor pl-2"><i class="fas fa-shopping-cart" id="logoi"></i> Kato</h2>
             </a>
-            <button class="btn ml-1 mr-2 f20 text-light d-sm-none d-block" data-toggle="tooltip" title="Logout">
+            <button class="btn ml-1 mr-1 f20 text-light d-sm-none d-block" data-toggle="tooltip" title="Logout">
                 <i class="fas fa-sign-out-alt"></i>
             </button>
         </div>
         <div class="col-sm-9 col-12" style="display: contents;">
-            <div class="input-group mr-3 ml-3 mb-1 m-sm-auto">
+            <div class="input-group mr-1 ml-1 mb-1 m-sm-auto">
                 <div class="input-group-prepend">
                     <select id="category" class="btn btn-outline-secondary text-light bg-dark" data-toggle="tooltip"
                         title="Category">
@@ -42,7 +42,7 @@ $homeUrl = $baseUrl;
             <button class="btn ml-2 mr-1 f20 text-light d-sm-block d-none" data-toggle="tooltip" title="Account">
                 <i class="fas fa-user"></i>
             </button>
-            <button class="btn ml-1 mr-2 f20 text-light d-sm-block d-none" data-toggle="tooltip" title="Logout">
+            <button class="btn ml-1 mr-1 f20 text-light d-sm-block d-none" data-toggle="tooltip" title="Logout">
                 <i class="fas fa-sign-out-alt"></i>
             </button>
         </div>
