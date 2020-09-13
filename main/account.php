4c10f399c2e2b2cef0731da7fb7ec0d02753f565
devblin <110119035@nitt.edu>
2020-09-14 00:36:17 +0530

    added

--- main/account.php
+++ main/account.php
@@ -1,37 +1,37 @@
 <div class="container p-0 ml-auto mr-auto mb-4 mt110" style="height: 80vh;">
-    <div style="    display: flex;
+    <div class="m-1" style="    display: flex;
     flex-direction: column;
     align-items: center;">
         <h3><i class="fas fa-user-circle"></i> Your Account</h3>
         <hr class="logobgcolor w-100">
-        <div class="input-group mb-3" style="max-width: 500px;">
-            <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-globe-americas"></i></span>
-            </div>
-            <input type="text" class="form-control" placeholder="Name">
-        </div>
-        <div class="input-group mb-3" style="max-width: 500px;">
-            <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-user"></i></span>
-            </div>
-            <input type="text" class="form-control" placeholder="Username">
-        </div>
-        <div class="input-group mb-3" style="max-width: 500px;">
-            <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
-            </div>
-            <input type="email" class="form-control" placeholder="Email">
+        <div class="input-group mb-3 d-block" style="max-width: 500px;">
+            <h5 data-toggle="tooltip" title="Your Name" class="text-primary"><i class="fas fa-globe"></i> <span
+                    id="accname">Deepanshu Dhruw</span></h5>
         </div>
         <div class="input-group mb-3 d-block" style="max-width: 500px;">
-            <h4><i class="fas fa-balance-scale"></i> Role <span class="text-primary">Seller</span></h4>
-            <!-- <div class="input-group-prepend">
-                <span class="input-group-text">@</span>
+            <h5 data-toggle="tooltip" title="Your Username" class="text-danger"><i class="fas fa-user-circle"></i> <span
+                    id="accuname">devblin</span></h5>
+        </div>
+        <div class="input-group mb-3" style="max-width: 300px;">
+            <div class="input-group-prepend">
+                <span class="input-group-text logobgcolor text-light"><i class="fas fa-envelope "></i></span>
             </div>
-            <input type="text" class="form-control" placeholder="Username"> -->
+            <input id="accemail" type="email" class="form-control" placeholder="Email">
+        </div>
+        <div class="input-group mb-3 d-block" style="max-width: 500px;">
+            <h4><i class="fas fa-balance-scale"></i> Role <span id="role" class="text-primary">Seller</span></h4>
         </div>
         <div>
-            <button class="btn btn-success w-auto"><i class="fas fa-pen-alt"></i> Update</button>
-            <button class="btn btn-danger w-auto"><i class="fas fa-trash-alt"></i> Delete Account</button>
+            <button id="accupdate" class="btn btn-success w-auto mb-1"><i class="fas fa-pen-alt"></i> Update</button>
+            <button id="accdelete" class="btn btn-danger w-auto mb-1"><i class="fas fa-trash-alt"></i> Delete
+                Account</button>
         </div>
     </div>
 </div>
+<script>
+class Account {
+    constructor() {
+        this.role
+    }
+}
+</script>
